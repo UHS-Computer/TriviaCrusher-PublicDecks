24159fb84e3c4dd86869f95d8ba1efbf96ce8d48
Peter Esser <me@peter-esser.de>
2023-12-01 16:10:08 +0100

    Add overview.json with automation script

---
+++ scripts/build-overview-file.py
@@ -0,0 +1,24 @@
+import glob
+import json_fix
+
+import json
+
+tableEntries = []
+
+# first, find all .tcjson files
+for f in glob.glob('./**/*.tcjson', recursive=True):
+    with open(f) as fi:
+        jsonString = fi.read()
+        # Convert JSON String to Python
+        deck = json.loads(jsonString)
+        tableEntries.append(
+            {
+                "name": deck["name"], "icon": deck["icon"], "description": deck["description"], "path": f, "language": f.split("/")[1]})
+
+print(tableEntries)
+
+# write overview file
+f = open("./overview.json", 'w')
+f.write(json.dumps({"foundLanguages": set(
+    [tableEntry["language"] for tableEntry in tableEntries]), "decks": tableEntries}, indent=2))
+f.close()
